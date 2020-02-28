b88068610d3981a75162e63cd73709cf5ef7c884
jay <jaykang1992@naver.com>
2020-02-27 21:19:55 -0600

    half working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import practiceCode from './practiceCode/code'
+import home from './home/home'
+import { Route, NavLink,HashRouter, Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background' >
+      <HashRouter>
+        <ul className='header'>
+          <li><NavLink to="/">About</NavLink></li>
+          <li><NavLink to="/code">Practice Code</NavLink></li>
+        </ul>
+        <div className='content'>
+          <Route exact path='/' component={home}></Route>
+          <Route path='/code' component={practiceCode}></Route>
+        </div>
+      </HashRouter>
     </div>
+    
   );
 }
 
---
+++ src/home/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './home.css';
+
+
+class home extends Component {
+    render(){
+        return (
+            <div className='about' >
+              This is home page
+            </div>
+            
+          );
+        }
+    }
+  
+export default home;
